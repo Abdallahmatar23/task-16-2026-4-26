b7a75264c028e2f1402be18e59d51ce6f6c0d6d0
abdallah <abdallahmatar10@gmail.com>
2026-05-05 00:36:49 +0300

    make view logic and handling

---
+++ index.php
@@ -0,0 +1,18 @@
+<form method="POST" action="session.php">
+    <div>
+        <label for="username">Enter Your Username : </label>
+    </div>
+    <div>
+        <input type="text" name="username" placeholder="Enter username">
+    </div>
+    <div>
+        <label for="username">Enter Your Password : </label>
+    </div>
+    <div>
+        <input type="password" name="password" placeholder="Enter password">
+    </div>
+
+    <div>
+        <button type="submit">Submit</button>
+    </div>
+</form>
--- session.php
+++ session.php
@@ -5,82 +5,119 @@ class Session
     // private string $name;
     // private string $sessionId;
     private static bool $started = false;
+    // public static function start()
+    // {
+    //     if (!self::$started) {
+    //         session_start();
+    //         self::$started = true;
+    //     } else {
+    //         echo "Session already started";
+    //     }
+    // }
     public static function start()
     {
-        if (!self::$started) {
+        if (session_status() === PHP_SESSION_NONE) {
             session_start();
-            self::$started = true;
-        } else {
-            echo "Session already started";
         }
     }
-    public static function setSession(string $key, string $value)
+    // public static function setSession(string $key, string $value)
+    // {
+    //     if (self::$started) {
+    //         if ($value !== null) {
+    //             $_SESSION[$key] = $value;
+    //         }
+    //     } else {
+    //         echo "Session Not Found";
+    //     }
+    // }
+    public static function setSession(string $key, array | string $value)
     {
-        if (self::$started) {
-            if ($value !== null) {
-                $_SESSION[$key] = $value;
-            }
-        } else {
-            echo "Session Not Found";
-        }
+        self::start();
+        $_SESSION[$key] = $value;
     }
     public static function getSession(string $key)
     {
-        if (self::$started) {
-            return $_SESSION[$key] ?? null;
-        } else {
-            echo "Session Not Found";
-        }
+        self::start();
+        return $_SESSION[$key] ?? null;
     }
     public static function flash(string $key)
     {
         // بترجع القيمه بتاع ال key لو موجوده وتعرضها وبعدين تمسحها
-        if (self::$started) {
-            if(isset($_SESSION[$key]))
+        self::start();
+        if (isset($_SESSION[$key])) {
             $msg = $_SESSION[$key];
             unset($_SESSION[$key]);
             return $msg;
-        } else {
-            echo "Session Not Found";
         }
+
+        return null;
     }
     public static function remove(string $key)
     {
-        if (self::$started) {
-            unset($_SESSION[$key]);
-        } else {
-            echo "Session not found";
-        }
+        self::start();
+        unset($_SESSION[$key]);
     }
     public static function removeAll()
     {
-        if (self::$started) {
-            session_unset();
-        } else {
-            echo "Session not found";
-        }
+        self::start();
+        session_unset();
     }
     public static function getAll()
     {
-        if (self::$started) {
-            return $_SESSION ?? null;
-        }
+        self::start();
+        return $_SESSION ?? null;
     }
     public static function has(string $key)
     {
+        self::start();
         return isset($_SESSION[$key]);
         // check exists true or false
     }
     public static function destroy()
     {
-        if (self::$started) {
+
+        if (session_status() !== PHP_SESSION_NONE) {
+            session_unset();
             session_destroy();
             self::$started = false;
-        } else {
-            echo "Session Not Found";
         }
     }
 }
 
-//form  and view sessions and apply methods
-// $data = $_POST;
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $data = $_POST;
+    // var_dump($data);
+
+    Session::start();
+
+    if (!empty($data['username']) && !empty($data['password'])) {
+        $username = trim($data['username']);
+        $pass = trim($data['password']);
+        Session::setSession('user', [
+            'username' => $username,
+            'password' => $pass
+        ]);
+    }
+    if (Session::has('user')) {
+        Session::setSession("success", "User Added Successfully");
+    } else {
+        Session::setSession("error", "Fail to add User");
+    }
+}
+
+
+if (Session::has('success')) {
+    echo Session::flash('success');
+    echo "</br>";
+}
+if (Session::has('error')) {
+    echo Session::flash('error');
+    echo "</br>";
+}
+if (Session::has('user')) {
+    $user = Session::getSession('user');
+    echo "Welcome " . htmlspecialchars($user['username']);
+    echo "</br>";
+    // بعرض الباسورد بجرب بس عارف انه غلط 😅
+    echo "Your Password is : " . htmlspecialchars($user['password']);
+}
