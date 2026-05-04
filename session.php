e8f09c50e2dcf4789f1f54c7d4a1754dc323d38e
abdallah <abdallahmatar10@gmail.com>
2026-05-04 23:29:51 +0300

    make class and methods

--- session.php
+++ session.php
@@ -1,19 +1,86 @@
 <?php
 
-class Session{
-    public function setSession($key,$value){}
-    public function getSession($key){
-        // return $value;
+class Session
+{
+    // private string $name;
+    // private string $sessionId;
+    private static bool $started = false;
+    public static function start()
+    {
+        if (!self::$started) {
+            session_start();
+            self::$started = true;
+        } else {
+            echo "Session already started";
+        }
     }
-    public function flash($key){
+    public static function setSession(string $key, string $value)
+    {
+        if (self::$started) {
+            if ($value !== null) {
+                $_SESSION[$key] = $value;
+            }
+        } else {
+            echo "Session Not Found";
+        }
+    }
+    public static function getSession(string $key)
+    {
+        if (self::$started) {
+            return $_SESSION[$key] ?? null;
+        } else {
+            echo "Session Not Found";
+        }
+    }
+    public static function flash(string $key)
+    {
         // بترجع القيمه بتاع ال key لو موجوده وتعرضها وبعدين تمسحها
+        if (self::$started) {
+            if(isset($_SESSION[$key]))
+            $msg = $_SESSION[$key];
+            unset($_SESSION[$key]);
+            return $msg;
+        } else {
+            echo "Session Not Found";
+        }
     }
-    public function remove($key){}
-    public function removeAll(){}
-    public function getAll(){}
-    public function has($key){
+    public static function remove(string $key)
+    {
+        if (self::$started) {
+            unset($_SESSION[$key]);
+        } else {
+            echo "Session not found";
+        }
+    }
+    public static function removeAll()
+    {
+        if (self::$started) {
+            session_unset();
+        } else {
+            echo "Session not found";
+        }
+    }
+    public static function getAll()
+    {
+        if (self::$started) {
+            return $_SESSION ?? null;
+        }
+    }
+    public static function has(string $key)
+    {
+        return isset($_SESSION[$key]);
         // check exists true or false
+    }
+    public static function destroy()
+    {
+        if (self::$started) {
+            session_destroy();
+            self::$started = false;
+        } else {
+            echo "Session Not Found";
+        }
     }
 }
 
 //form  and view sessions and apply methods
+// $data = $_POST;
